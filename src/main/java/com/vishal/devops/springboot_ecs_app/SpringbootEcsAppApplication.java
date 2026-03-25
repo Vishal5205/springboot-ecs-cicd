7599dbe7afa3f590177cb4a0151dc4862ead21e7
Vishal5205 <vishalaws87@gmail.com>
2026-03-25 16:23:22 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/vishal/devops/springboot_ecs_app/SpringbootEcsAppApplication.java
@@ -0,0 +1,13 @@
+package com.vishal.devops.springboot_ecs_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEcsAppApplication.class, args);
+	}
+
+}
